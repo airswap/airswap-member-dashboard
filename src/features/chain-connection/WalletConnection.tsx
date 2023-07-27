49dd4e86723cb36de6157c0d17d59278850c4712
Mike Starr <imhs@protonmail.com>
2023-07-27 11:58:09 -0400

    refactors including consolidating WalletConnection and Modal

--- src/features/chain-connection/WalletConnection.tsx
+++ src/features/chain-connection/WalletConnection.tsx
@@ -1,31 +1,81 @@
-import { useAccount, useEnsName, useDisconnect } from "wagmi";
+import { useAccount, useEnsName, useDisconnect, useConnect, Connector } from "wagmi";
 import { Button } from "../common/Button";
 import { twJoin } from "tailwind-merge";
 import truncateEthAddress from "truncate-eth-address";
-import { FC, Dispatch } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { useEffect } from "react";
 
-interface WalletConnectionProps {
-  setRenderWalletConnectModal: Dispatch<boolean>
-}
-
-export const WalletConnection: FC<WalletConnectionProps> = ({ setRenderWalletConnectModal }) => {
+const WalletConnection = () => {
   const { address, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
-
+  const { connect, connectors, isLoading, pendingConnector } = useConnect()
   const { disconnect } = useDisconnect()
 
+  const dialog = document.querySelector('dialog')
+
+  useEffect(() => {
+    if (isConnected) {
+      dialog?.close()
+    }
+  }, [isConnected, dialog])
+
+  const connectorButtons = () => {
+    return connectors.map((connector: Connector) => {
+      return (
+        <button
+          className="flex flex-row items-center p-2 border-2 border-border-dark rounded"
+          disabled={!connector.ready}
+          onClick={() => connect({ connector })}
+          key={connector.name}
+        >
+          <img
+            src={`src/assets/${connector.id}-logo.svg`}
+            alt='MetaMask logo'
+            className="mr-4 w-8 h-8"
+          />
+          <span className="color-white">
+            {connector.name}
+            {!connector.ready && ' (unsupported)'}
+            {isLoading &&
+              connector.id === pendingConnector?.id &&
+              ' (connecting)'}
+          </span>
+        </button>)
+    })
+  }
+
   return (
-    <Button
-      className={twJoin([
-        "flex flex-row items-center gap-2",
-        isConnected && "cursor-default",
-      ])}
-      onClick={() => !isConnected ? setRenderWalletConnectModal(true) : disconnect()}
-    >
-      <div className="h-3 w-3 rounded-full bg-accent-green"></div>
-      <span className="font-medium">
-        {isConnected ? ensName || truncateEthAddress(address || "") : "Connect"}
-      </span>
-    </Button>
+    <>
+      <Button
+        className={twJoin([
+          "flex flex-row items-center gap-2",
+          isConnected && "cursor-default",
+        ])}
+        onClick={() => !isConnected ? dialog?.showModal() : disconnect()}
+      >
+        <div className="h-3 w-3 rounded-full bg-accent-green"></div>
+        <span className="font-medium">
+          {isConnected ? ensName || truncateEthAddress(address || "") : "Connect"}
+        </span>
+      </Button>
+
+      <dialog className="rounded-md backdrop:bg-color-black color-white">
+        <div
+          className="flex flex-col space-y-3 bg-bg-dark px-6 pt-4 pb-6 font-bold w-[360px] color-white"
+        >
+          <div className="flex flex-row px-2 pb-1  justify-between">
+            <span>Select Wallet</span>
+            <button
+              onClick={() => dialog?.close()}
+            >
+              <AiOutlineClose />
+            </button>
+          </div>
+          {connectorButtons()}
+        </div>
+      </dialog>
+    </>
   );
 };
+
+export default WalletConnection
--- src/features/structure/Header.tsx
+++ src/features/structure/Header.tsx
@@ -1,26 +1,11 @@
 import AirSwapLogo from "../../assets/airswap-logo.svg";
 import AirSwapLogoWithText from "../../assets/airswap-logo-with-text.svg";
 import { SettingsMenuButton } from "../settings/SettingsMenuButton";
-import { WalletConnection } from "../chain-connection/WalletConnection";
+import WalletConnection from "../chain-connection/WalletConnection";
 import { StakeButton } from "../staking/StakeButton";
-import { useEffect, useState } from "react";
-import WalletConnectModal from "../../components/WalletConnectModal";
-import { useAccount } from "wagmi";
+
 
 export const Header = ({ }: {}) => {
-  const [isRenderWalletConnectModal, setIsRenderWalletConnectModal] = useState<boolean>(false)
-  const { isConnected } = useAccount();
-
-  const handleCloseModal = (): void => {
-    setIsRenderWalletConnectModal(false)
-  }
-
-  useEffect(() => {
-    if (!isConnected) {
-      setIsRenderWalletConnectModal(false)
-    }
-  }, [isConnected])
-
   return (
     <div className="flex h-24 flex-row items-center justify-between px-8">
       <div>
@@ -33,8 +18,7 @@ export const Header = ({ }: {}) => {
       </div>
 
       <div className="flex flex-row items-center gap-4">
-        <WalletConnection setRenderWalletConnectModal={setIsRenderWalletConnectModal} />
-        <WalletConnectModal isDisplayModal={isRenderWalletConnectModal} onClose={handleCloseModal} />
+        <WalletConnection />
         <SettingsMenuButton />
         <StakeButton />
       </div>
