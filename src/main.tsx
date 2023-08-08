9ef919d58e4b8e7addb8036f4899fbbe1f1662fb
greypixel <greypixel@gpxl.tech>
2023-08-08 15:39:13 +0100

    Fix wallet logos, add support for rabby

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { WagmiConfig, configureChains, createConfig, mainnet } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-import App from "./App.tsx";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { goerli } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import App from "./App.tsx";
+import AirSwapLogo from "./assets/airswap-logo.svg";
+import "./index.css";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, goerli],
   [
     alchemyProvider({ apiKey: import.meta.env.VITE_ALCHEMY_API_KEY || "" }),
-    publicProvider()
+    publicProvider(),
   ],
 );
 
@@ -28,13 +31,23 @@ const config = createConfig({
   queryClient,
   connectors: [
     new MetaMaskConnector({ chains }),
+    new InjectedConnector({
+      chains,
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "AirSwap Voter Rewards",
+        appLogoUrl: AirSwapLogo,
+      },
+    }),
     new WalletConnectConnector({
       chains,
       options: {
-        projectId: import.meta.env.VITE_WALLETCONNECT_ID
-      }
-    })
-  ]
+        projectId: import.meta.env.VITE_WALLETCONNECT_ID,
+      },
+    }),
+  ],
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,5 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_SNAPSHOT_HUB_GRAPHQL_ENDPOINT?: string;
   readonly VITE_SNAPSHOT_SPACE?: string;
   readonly VITE_SNAPSHOT_WEB?: string;
+  readonly VITE_WALLETCONNECT_ID?: string;
   // more env variables...
 }
