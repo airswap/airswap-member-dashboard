1cf449edb2cb009b9dfd8bb5f879e084ff306d34
gpxl-dev <greypixel@gpxl.tech>
2023-08-23 13:21:05 +0100

    Set root button (#34)
    * Add isAdmin hook
    * Set root button

--- src/features/votes/hooks/useGroupMerkleRoot.ts
+++ src/features/votes/hooks/useGroupMerkleRoot.ts
@@ -5,11 +5,13 @@ export const useGroupMerkleRoot = (
   proposalIds: string[],
   options?: { enabled: boolean },
 ) => {
-  const tree = useGroupMerkleTree(proposalIds);
+  const tree = useGroupMerkleTree(proposalIds, {
+    enabled: options?.enabled ?? true,
+  });
 
   return useQuery(
     ["group-merkle-root", proposalIds],
-    () => tree!.getHexRoot(),
+    () => tree!.getHexRoot() as `0x${string}`,
     {
       enabled: (options?.enabled ?? true) && !!tree,
       cacheTime: Infinity,
