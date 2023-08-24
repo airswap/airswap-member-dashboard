aa02a491ab706364fca8d54437d8e37710ad9e38
greypixel <greypixel@gpxl.tech>
2023-08-24 16:46:41 +0100

    Update pool contract

--- src/config/ContractAddresses.ts
+++ src/config/ContractAddresses.ts
@@ -11,7 +11,7 @@ export const contractAddressesByChain: Record<number, ContractList> = {
     [ContractTypes.AirSwapToken]: "0x27054b13b1B798B345b591a4d22e6562d47eA75a",
     [ContractTypes.AirSwapStaking]:
       "0x9fc450F9AfE2833Eb44f9A1369Ab3678D3929860",
-    [ContractTypes.AirSwapPool]: "0xe2E7AE67E7ee6d4D90dfef945aB6dE6A14dB4c17",
+    [ContractTypes.AirSwapPool]: "0xFeEFD5F62A10277fF8082a80735a8380033439d0",
   },
   5: {
     [ContractTypes.AirSwapToken]: "0x1a1ec25DC08e98e5E93F1104B5e5cdD298707d31",
