31c6bb316f174a4ebe7071d84bdb9b06dcfca2ea
Mike Starr <90465420+personnamedmike@users.noreply.github.com>
2023-08-30 12:29:30 -0400

    Fix stakableBar component (#45)
    * Added 3% flexBasis as padding to maintain border-radius in case of 0 values in StakableBar
    
    * replaced +3% in flexBasis with min-w[3%]

--- src/features/staking/subcomponents/StakableBar.tsx
+++ src/features/staking/subcomponents/StakableBar.tsx
@@ -1,22 +1,16 @@
-import { FC } from "react";
-import { calculateTokenProportions } from "../utils/calculateTokenProportions";
-import { twJoin } from "tailwind-merge";
 import { BsCircleFill } from "react-icons/bs";
-import "../../../index.css";
 import { useTokenBalances } from "../../../hooks/useTokenBalances";
-
-interface StakableBarProps {
-  mode: "stake" | "unstake";
-}
+import "../../../index.css";
+import { calculateTokenProportions } from "../utils/calculateTokenProportions";
 
 /**
- *
+
  * @param unstakable - balance of sAST available to unstake
  * @param staked - total amount of SAST
  * @param stakable - amount of available AST
- * @returns
+ * @returns component that displays a visual representation of available token balances
  */
-export const StakableBar: FC<StakableBarProps> = ({ mode }) => {
+export const StakableBar = () => {
   const {
     ustakableSAstBalanceFormatted: unstakable,
     sAstBalanceFormatted: staked,
@@ -32,53 +26,35 @@ export const StakableBar: FC<StakableBarProps> = ({ mode }) => {
 
   return (
     <div className="flex w-full flex-col space-y-3">
-      {mode === "stake" && (
-        <>
-          <div className="m-auto mt-6 flex h-3 w-full flex-row rounded-full">
-            <div
-              style={{ flexBasis: `${unstakablePercent}%` }}
-              className="checkered-blue rounded-l-full"
-            ></div>
-            <div
-              style={{ flexBasis: `${stakedPercent}%` }}
-              className="bg-accent-blue"
-            ></div>
-            <div
-              style={{ flexBasis: `${stakablePercent}%` }}
-              className="rounded-r-full bg-accent-gray"
-            ></div>
-          </div>
-          <div className="flex flex-row items-center">
-            <div className="checkered-blue rounded-full">
-              <BsCircleFill className="text-transparent" />
-            </div>
-            <span className="mx-2">{unstakable}</span>
-            unstakable
-          </div>
-          <div className="flex flex-row items-center">
-            <BsCircleFill className="text-blue-500" />
-            <span className="mx-2">{staked}</span>staked
-          </div>
-          <div className="flex flex-row items-center">
-            <BsCircleFill className="text-accent-gray" />
-            <span className="mx-2">{stakable}</span>stakable
-          </div>
-        </>
-      )}
-      {mode === "unstake" && (
-        <>
-          <div className="m-auto mt-6 flex h-3 w-[100%] flex-row rounded-full">
-            <div
-              className={twJoin(["w-full rounded-full bg-accent-gray"])}
-              // style={{ flexBasis: `${stakablePercent}%` }}
-            ></div>
-          </div>
-          <div className="flex flex-row items-center">
-            <BsCircleFill className="text-accent-gray" />
-            <span className="mx-2">{stakable}</span>stakable
-          </div>
-        </>
-      )}
+      <div className="m-auto mt-6 flex h-3 w-full flex-row rounded-full">
+        <div
+          style={{ flexBasis: `${unstakablePercent}%` }}
+          className="checkered-blue rounded-l-full min-w-[3%]"
+        ></div>
+        <div
+          style={{ flexBasis: `${stakedPercent}%` }}
+          className="bg-accent-blue"
+        ></div>
+        <div
+          style={{ flexBasis: `${stakablePercent}%` }}
+          className="rounded-r-full bg-accent-gray min-w-[3%]"
+        ></div>
+      </div>
+      <div className="flex flex-row items-center">
+        <div className="checkered-blue rounded-full">
+          <BsCircleFill className="text-transparent" />
+        </div>
+        <span className="mx-2">{unstakable}</span>
+        unstakable
+      </div>
+      <div className="flex flex-row items-center">
+        <BsCircleFill className="text-blue-500" />
+        <span className="mx-2">{staked}</span>staked
+      </div>
+      <div className="flex flex-row items-center">
+        <BsCircleFill className="text-accent-gray" />
+        <span className="mx-2">{stakable}</span>stakable
+      </div>
     </div>
   );
 };
