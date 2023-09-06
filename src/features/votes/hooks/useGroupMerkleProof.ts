5a503bc1c37c7ca3b680c34e75cffc635c1b5488
greypixel <greypixel@gpxl.tech>
2023-09-06 12:37:52 +0100

    Prevent infinite render

--- src/features/votes/hooks/useGroupMerkleProof.ts
+++ src/features/votes/hooks/useGroupMerkleProof.ts
@@ -18,7 +18,7 @@ export const useGroupMerkleProof = ({
 
   return useMemo(() => {
     if (!tree) return;
-    const leaf = generateMerkleLeaf(vote);
+    const leaf = generateMerkleLeaf({ voter: vote.voter, vp: vote.vp });
     return tree.getHexProof(leaf) as `0x${string}`[];
-  }, [tree, vote]);
+  }, [tree, vote.voter, vote.vp]);
 };
