1cef422d03c1dbce2cb289ff1aaa22ee093e241c
greypixel <greypixel@gpxl.tech>
2023-09-06 12:26:28 +0100

    Better varaible name

--- src/features/claims/hooks/useResetClaimStatus.tsx
+++ src/features/claims/hooks/useResetClaimStatus.tsx
@@ -1,18 +1,18 @@
 import { useAccount, useQueryClient } from "wagmi";
 
-export const useResetClaimStatus = (proposalIds: `0x${string}`[] = []) => {
+export const useResetClaimStatus = (treeId: `0x${string}`[] = []) => {
   const queryClient = useQueryClient();
   const { address } = useAccount();
 
   return () =>
-    proposalIds.length &&
+    treeId.length &&
     queryClient.resetQueries({
       predicate: (query) => {
         if (Array.isArray(query.queryKey)) {
           return (
             query.queryKey[0].args?.[1] === address &&
             query.queryKey[0].functionName === "claimed" &&
-            proposalIds.includes(query.queryKey[0].args?.[0])
+            treeId.includes(query.queryKey[0].args?.[0])
           );
         } else {
           return false;
