3f614d680e7852f3971b24ec9991b6c0da83d6f9
Mike Starr <90465420+personnamedmike@users.noreply.github.com>
2023-09-07 15:21:45 -0400

    hook switches user to Mainnet if invalid chain is connected (#52)
    * hook switches user to Mainnet if invalid chain is connected
    
    * useSwitchNetwork wagmi hook for network switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+import { useNetwork, useSwitchNetwork } from "wagmi";
 import { Header } from "./features/structure/Header";
 import { VoteList } from "./features/votes/VoteList";
 import { useApplyTheme } from "./hooks/useApplyTheme";
 
 function App() {
   useApplyTheme();
+  const { chain } = useNetwork();
+
+  const { switchNetwork } = useSwitchNetwork({
+    throwForSwitchChainNotSupported: true,
+  });
+
+  useEffect(() => {
+    if (chain?.id === 1 || chain?.id === 5) {
+      null;
+    } else {
+      switchNetwork && switchNetwork(1);
+    }
+  }, [chain?.id, switchNetwork]);
 
   return (
     <div className="flex flex-col flex-1 h-full">
