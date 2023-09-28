daf8cd69dd455c17957357662f697e8aebdf182f
greypixel <greypixel@gpxl.tech>
2023-09-29 00:54:58 +0100

    Conditionally render only when claim exists

--- src/features/activate-migration/ActivatePointsCard.tsx
+++ src/features/activate-migration/ActivatePointsCard.tsx
@@ -1,4 +1,5 @@
 import { format } from "@greypixel_/nicenumbers";
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { twJoin, twMerge } from "tailwind-merge";
 import { Checkbox } from "../common/Checkbox";
@@ -49,53 +50,65 @@ export const ActivatePointsCard = ({}: {}) => {
   ]);
 
   return (
-    <div className="w-full items-center border border-gray-800 rounded p-4 pl-5">
-      <div className="flex w-full items-center justify-between pr-4 font-semibold">
-        {/* Checkbox and title. */}
-        <div className="flex items-center">
-          {/* Checkbox */}
-          <div className="align-center -mt-1 mr-5 items-center ">
-            <Checkbox
-              disabled={
-                hasUserClaimed // or if the user has already claimed
-              }
-              checked={isClaimSelected(ACTIVATE_TREE_ID)}
-              onCheckedChange={(newState) => {
-                // Button will be disabled if there's no claim, so ! is ok.
-                setClaimSelected(activateMigrationClaim!, newState as boolean);
-              }}
-            />
+    activateMigrationClaim && (
+      <motion.div
+        className="w-full items-center border border-gray-800 rounded p-4 pl-5"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        <div className="flex w-full items-center justify-between pr-4 font-semibold">
+          {/* Checkbox and title. */}
+          <div className="flex items-center">
+            {/* Checkbox */}
+            <div className="align-center -mt-1 mr-5 items-center ">
+              <Checkbox
+                disabled={
+                  hasUserClaimed // or if the user has already claimed
+                }
+                checked={isClaimSelected(ACTIVATE_TREE_ID)}
+                onCheckedChange={(newState) => {
+                  // Button will be disabled if there's no claim, so ! is ok.
+                  setClaimSelected(
+                    activateMigrationClaim!,
+                    newState as boolean,
+                  );
+                }}
+              />
+            </div>
+            {/* Title */}
+            <div
+              className={twMerge(
+                "font-bold",
+                hasUserClaimed && "text-gray-500",
+              )}
+            >
+              Points from Activate
+            </div>
           </div>
-          {/* Title */}
-          <div
-            className={twMerge("font-bold", hasUserClaimed && "text-gray-500")}
-          >
-            Points from Activate
-          </div>
-        </div>
 
-        {/* Points */}
-        <div
-          className={twJoin([
-            "rounded-full px-4 py-1 text-xs font-bold uppercase leading-6",
-            "flex flex-row items-center gap-2 ring-1 ring-gray-800",
-            hasUserClaimed && "text-gray-500",
-          ])}
-        >
-          {hasUserClaimed && (
-            <span className="text-green-400">
-              <CheckMark size={20} />
-            </span>
-          )}
-          {/* TODO: small numbers of points probably don't need decimals. */}
-          {format(activateMigrationClaim?.value, {
-            tokenDecimals: 0,
-            significantFigures: 3,
-            minDecimalPlaces: 0,
-          })}
-          &nbsp; Points {hasUserClaimed && " claimed "}
+          {/* Points */}
+          <div
+            className={twJoin([
+              "rounded-full px-4 py-1 text-xs font-bold uppercase leading-6",
+              "flex flex-row items-center gap-2 ring-1 ring-gray-800",
+              hasUserClaimed && "text-gray-500",
+            ])}
+          >
+            {hasUserClaimed && (
+              <span className="text-green-400">
+                <CheckMark size={20} />
+              </span>
+            )}
+            {/* TODO: small numbers of points probably don't need decimals. */}
+            {format(activateMigrationClaim?.value, {
+              tokenDecimals: 0,
+              significantFigures: 3,
+              minDecimalPlaces: 0,
+            })}
+            &nbsp; Points {hasUserClaimed && " claimed "}
+          </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    )
   );
 };
