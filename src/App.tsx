ccf71987ea341983ee3584bd104c6f5c32ae8446
greypixel <greypixel@gpxl.tech>
2023-10-09 13:08:38 +0100

    Add wallet disconnected state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useAccount } from "wagmi";
+import { DisconnectedState } from "./features/structure/DisconnectedState";
 import { Header } from "./features/structure/Header";
 import { ClaimFloat } from "./features/votes/ClaimFloat";
 import { VoteList } from "./features/votes/VoteList";
@@ -7,12 +9,13 @@ import { useApplyTheme } from "./hooks/useApplyTheme";
 function App() {
   useApplyTheme();
   const { setShowClaimModal } = useClaimSelectionStore();
+  const { isConnected } = useAccount();
 
   return (
     <div className="flex flex-col flex-1 h-full items-center">
       <Header />
       <main className="mx-auto flex w-[808px] p-2 max-w-full flex-col flex-1">
-        <VoteList />
+        {isConnected ? <VoteList /> : <DisconnectedState />}
       </main>
       <ClaimFloat onClaimClicked={() => setShowClaimModal(true)} />
     </div>
