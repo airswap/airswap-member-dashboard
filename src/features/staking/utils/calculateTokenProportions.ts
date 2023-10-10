c7669ab5de1f5bdf5cb365bf3ab4f56120d42bd0
Mike Starr <imhs@protonmail.com>
2023-10-10 10:16:44 -0400

    fixed proportions and display on piebar

--- src/features/staking/utils/calculateTokenProportions.ts
+++ src/features/staking/utils/calculateTokenProportions.ts
@@ -1,4 +1,5 @@
 type PercentObject = {
+  totalStakedPercent: number;
   unstakablePercent: number;
   stakedPercent: number;
   unstakedPercent: number;
@@ -9,27 +10,37 @@ export const calculateTokenProportions = ({
   staked,
   unstaked,
 }: {
-  unstakable: number;
-  staked: number;
-  unstaked: number;
+  unstakable: bigint;
+  staked: bigint;
+  unstaked: bigint;
 }): PercentObject => {
-  const totalBalance = staked + unstaked;
+  const unstakableNum = Number(unstakable);
+  const stakedNum = Number(staked);
+  const unstakedNum = Number(unstaked);
+  const totalBalance = stakedNum + unstakedNum;
+
+  console.log(totalBalance);
 
   if (totalBalance === 0) {
     return {
+      totalStakedPercent: 0,
       unstakablePercent: 0,
       stakedPercent: 0,
       unstakedPercent: 0,
     };
   }
 
-  const unstakablePercent = (unstakable / totalBalance) * 100;
+  const unstakedPercent = (unstakedNum / totalBalance) * 100;
 
-  const stakedTotalPercent = 100 - unstakablePercent;
-  const stakedPercent = (staked / stakedTotalPercent) * 100;
-  const unstakedPercent = (unstaked / stakedTotalPercent) * 100;
+  // this number gets passed into a div which wraps staked and unstakable
+  const totalStakedPercent = 100 - unstakedPercent;
+  // this is a percentage of totalStakedPercent
+  const unstakablePercent = (unstakableNum / stakedNum) * 100;
+  // this is a percentage of totalStakedPercent
+  const stakedPercent = 100 - unstakablePercent;
 
   return {
+    totalStakedPercent,
     unstakablePercent,
     stakedPercent,
     unstakedPercent,
