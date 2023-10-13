e3713f5b54dc2d2bc7875658b1e5e447967fee2d
Don Mosites <mosites@gmail.com>
2023-10-13 15:20:02 +0200

    update pool addresses

--- src/config/ContractAddresses.ts
+++ src/config/ContractAddresses.ts
@@ -11,25 +11,25 @@ export const contractAddressesByChain: Record<number, ContractList> = {
     [ContractTypes.AirSwapToken]: "0x27054b13b1B798B345b591a4d22e6562d47eA75a",
     [ContractTypes.AirSwapStaking]:
       "0x9fc450F9AfE2833Eb44f9A1369Ab3678D3929860",
-    [ContractTypes.AirSwapPool]: "0xEEcD248D977Fd4D392915b4AdeF8154BA3aE9c02",
+    [ContractTypes.AirSwapPool]: "0xbbcec987e4c189fcbab0a2534c77b3ba89229f11",
   },
   5: {
     // goerli
     [ContractTypes.AirSwapToken]: "0x1a1ec25DC08e98e5E93F1104B5e5cdD298707d31",
     [ContractTypes.AirSwapStaking]:
       "0x51F372bE64F0612532F28142cECF8F204B272622",
-    [ContractTypes.AirSwapPool]: "0xEEcD248D977Fd4D392915b4AdeF8154BA3aE9c02",
+    [ContractTypes.AirSwapPool]: "0xbbcec987e4c189fcbab0a2534c77b3ba89229f11",
   },
   56: {
     // bsc
-    [ContractTypes.AirSwapPool]: "0xEEcD248D977Fd4D392915b4AdeF8154BA3aE9c02",
+    [ContractTypes.AirSwapPool]: "0xbbcec987e4c189fcbab0a2534c77b3ba89229f11",
   },
   137: {
     // polygon
-    [ContractTypes.AirSwapPool]: "0xEEcD248D977Fd4D392915b4AdeF8154BA3aE9c02",
+    [ContractTypes.AirSwapPool]: "0xbbcec987e4c189fcbab0a2534c77b3ba89229f11",
   },
   43114: {
     // avax
-    [ContractTypes.AirSwapPool]: "0xEEcD248D977Fd4D392915b4AdeF8154BA3aE9c02",
+    [ContractTypes.AirSwapPool]: "0xbbcec987e4c189fcbab0a2534c77b3ba89229f11",
   },
 };
